c10d15031acd2f4de4edf4dbe4f1e86c9fdffe71
Andrei <a.prudniks@gmail.com>
2021-10-26 18:05:58 +0300

    by lesson

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,7 +5,7 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
             Console.WriteLine($"Hello, {args[0]}!");
         }
     }
